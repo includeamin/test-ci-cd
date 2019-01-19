381a3f0d0e7d22c77025deebaed330849f751c6d
aminjamal <aminjamal10@gmail.com>
2019-01-19 17:03:18 +0430

    amin

--- app.py
+++ app.py
@@ -13,8 +13,12 @@ def hello():
 @app.route("/amin")
 def amin():
     return "check mail"
-
-
+@app.route("/body" ,methods=["POST"])
+def body():
+    return request.form["Token"]
+@app.route("/header")
+def header():
+    return request.headers["Token"]
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3007)
 
